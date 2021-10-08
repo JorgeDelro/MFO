facb087f22b7b9c3f3cc2e41eec3dc48ef5cbd63
Jorge <delrito84@hotmail.com>
2021-10-08 09:35:03 +0200

    First half of the package done

---
+++ R/MFO.R
@@ -0,0 +1,147 @@
+
+#' Maximal Fat Oxidation Function
+#'
+#' @param step_time how often the data was collected (in seconds).
+#' @param db_MFO database containing MFO test.
+#' @param db_basal database containing basal test.
+#' @param db_graded database containing incremental exercise test.
+#' @param cv_var variable to estimate coefficient of variation. Can be: VO2, VCO2 or RER.
+#' @param author author to estimate MFO. Can be: Frayn or Jeukendrup.
+#' @param VO2max VO2max can be passed directly using this argument instead of use db_graded argument.
+#'
+#' @import ggplot2
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Path to the MFO package sample data
+#' path <- system.file("extdata", "sample_data.xlsx", package = "MFO")
+#'
+#' # Read databases
+#' sample_data <- read_MFO_databases(from = "files",
+#'                                  path = paste(path),
+#'                                  db_basal_name = "M.BASAL",
+#'                                  db_MFO_name = "MFO",
+#'                                  db_graded_name = "V02máx.",
+#'                                  col_name_VO2 = "V'O2",
+#'                                  col_name_VCO2 = "V'CO2",
+#'                                  col_name_RER = "RER",
+#'                                  col_name_HR = "HR",
+#'                                  remove_rows = NULL)
+#' # Calculate MFO and Fatmax
+#' result_MFO <- MFO(step_time = 20,
+#'                  db_MFO = sample_data$participant_db_MFO,
+#'                  db_basal = sample_data$participant_db_basal,
+#'                  db_graded = sample_data$participant_db_graded,
+#'                  cv_var = "RER",
+#'                  author = "Frayn",
+#'                  VO2max = NULL)
+#'
+#' }
+MFO <- function(step_time,
+                db_MFO,
+                db_basal,
+                db_graded = NULL,
+                cv_var,
+                author,
+                VO2max = NULL) {
+
+  # Get VO2max
+  if(!is.null(db_graded)) {
+    VO2max <- max(db_graded["VO2"])
+  } else if(is.null(db_graded)) {
+    VO2max <- VO2max
+  } else if(is.null(db_graded) & is.null(VO2max)) {
+    stop("you must provide a VO2max value using db_graded or VO2max arguments")
+  }
+
+  # Get basal data
+  basal <- met_basal(step_time = step_time,
+                     db = db_basal,
+                     cv_var = cv_var)
+
+  # Calculate CHO, FAT and Kcal - Frayn or Jeukendrup
+  # calculate_vars function
+  MFO_test <- calculate_vars(step_time = step_time,
+                             db_MFO = db_MFO,
+                             VO2max = VO2max,
+                             author = author)
+
+  # Create db for graph
+  # Load
+  Load <- c(1:dim(MFO_test)[1] * 15)
+  Load <- c("basal", Load)
+
+  # VO2
+  VO2 <- c()
+  for (i in 1:dim(MFO_test)[1]) {
+    VO2[i] <- MFO_test[i, "VO2"]
+  }
+  VO2 <- c(basal$x_VO2, VO2)
+
+  # FAT
+  FAT <- c()
+  for (i in 1:dim(MFO_test)[1]) {
+    FAT[i] <- MFO_test[i, "FAT"]
+  }
+  FAT <- c(basal$x_FAT, FAT)
+
+
+  MFO_db <- tibble(Load = Load,
+                   VO2 = VO2,
+                   FAT = FAT) %>%
+    mutate(porc_VO2 = (VO2 * 100) /  VO2max)
+
+  # Remove NA´s
+  MFO_db <- na.omit(MFO_db)
+  # Convert negative number to 0
+  MFO_db[MFO_db$FAT <= 0, 3] <- 0
+
+  # Polynomial regression
+  MFO_mod <- lm(MFO_db$FAT ~ MFO_db$porc_VO2 + I(MFO_db$porc_VO2^2))
+
+  # FATmax
+  VO2_MFO <- (MFO_mod$coefficients[[2]] / (2 * MFO_mod$coefficients[[3]]))*-1
+  VO2_MFO_text <- paste("FAT[MAX] == ~",round(VO2_MFO, 2))
+  # MFO
+  VAR_MFO <- MFO_mod$coefficients[[3]] * (VO2_MFO^2) + (MFO_mod$coefficients[[2]] * VO2_MFO) + MFO_mod$coefficients[[1]]
+  VAR_MFO_text <- paste("MFO == ~",round(VAR_MFO, 2))
+
+  # MFO plot
+  MFO_plot <- MFO_db %>%
+    ggplot(aes(x = porc_VO2, y = FAT)) +
+    geom_point(aes(size = 15)) +
+    stat_smooth(method='lm', formula = y~x + I(x^2), se = F) +
+    ylab("FAT (gr)") +
+    xlab(expression("Exercise " *"intensity " *"("*VO["2max"]*")")) +
+    theme_bw() +
+    theme(legend.position = "none",
+          axis.text.x = element_text(size=11),
+          axis.text.y = element_text(size=11),
+          axis.title = element_text(size=12)) +
+    annotate("label",
+             #x = min(MFO_db$porc_VO2) + 5,
+             #y = max(MFO_db$FAT),
+             x=Inf,
+             y = Inf,
+             vjust=1.5,
+             hjust=1.25,
+             label = VO2_MFO_text,
+             parse = T) +
+    annotate("label",
+             #x = min(MFO_db$porc_VO2) + 5,
+             #y = max(MFO_db$FAT) - 0.05,
+             x=Inf,
+             y = Inf,
+             vjust=3.5,
+             hjust=1.25,
+             label = VAR_MFO_text,
+             parse = T)
+
+  return(list(MFO_db = MFO_db,
+              MFO_plot = MFO_plot,
+              MFO = VAR_MFO,
+              FAT_MAX = VO2_MFO,
+              x_CHO = basal$x_CHO,
+              x_FAT = basal$x_FAT,
+              x_Kcal = basal$x_Kcal))
---
+++ R/utils.R
@@ -0,0 +1,242 @@
+
+#' Basal metabolic rate
+#'
+#' @param step_time how often the data was collected (in seconds).
+#' @param db a database
+#' @param cv_var variable to calculate coefficient of variation
+#'
+#' @importFrom dplyr pull mutate
+#' @importFrom tibble add_column
+#'
+met_basal <- function(step_time, db, cv_var) {
+
+  n_row <- calculate_steps(step_time = step_time,
+                           db = NULL,
+                           db_type = "basal")
+
+  # First rows
+  cv_VO2_1 <- rep(NA, n_row)
+  cv_VCO2_1 <- rep(NA, n_row)
+  cv_RER_1 <- rep(NA, n_row)
+
+  n_row <- n_row + 1
+
+  # Empty vectors
+  cv_VO2_2 <- rep(NA, length(pull(db["VO2"])) - n_row)
+  cv_VCO2_2 <- rep(NA, length(pull(db["VCO2"])) - n_row)
+  cv_RER_2 <- rep(NA, length(pull(db["RER"])) - n_row)
+
+  # VO2
+  ############ VO2
+  j <- 1
+  for (i in n_row:length(pull(db["VO2"]))) {
+    cv_VO2_2[j] <- sd(as.vector(db[j:i, "VO2"])) / mean(as.vector(db[j:i, "VO2"]))
+    j <- j + 1
+  }
+
+  ############ VCO2
+  j <- 1
+  for (i in n_row:length(pull(db["VCO2"]))) {
+    cv_VCO2_2[j] <- sd(as.vector(db[j:i, "VCO2"])) / mean(as.vector(db[j:i, "VCO2"]))
+    j <- j + 1
+  }
+
+  ############ RER
+  j <- 1
+  for (i in n_row:length(pull(db["RER"]))) {
+    cv_RER_2[j] <- sd(as.vector(db[j:i, "RER"])) / mean(as.vector(db[j:i, "RER"]))
+    j <- j + 1
+  }
+
+  # Merge variables
+  cv_VO2 <- round(c(cv_VO2_1, cv_VO2_2*100),2)
+  cv_VCO2 <- round(c(cv_VCO2_1, cv_VCO2_2*100),2)
+  cv_RER <- round(c(cv_RER_1, cv_RER_2*100),2)
+
+  # Add columns to db
+  db <- db %>%
+    add_column(cv_VO2, cv_VCO2, cv_RER)
+
+  # get 5 min db
+  cv_var <- paste("cv_", cv_var, sep = "")
+  db_5min <- get_5min(db, cv_var = cv_var, n_row = n_row)
+
+  db_5min <- db_5min %>%
+    mutate(CHO_Frayn = (4.55 * VCO2 - 3.21 * VO2) / 1000,
+           FAT_Frayn = (1.67 * VO2 - 1.67 * VCO2) / 1000,
+           Kcal_total_Frayn = CHO_Frayn * 4 + FAT_Frayn * 9)
+
+  ## Return
+  # mean VO2
+  x_VO2 <- mean(pull(db_5min["VO2"]))
+  # mean HR
+  x_HR <- mean(pull(db_5min["HR"]))
+  # mean RER
+  x_RER <- mean(pull(db_5min["RER"]))
+  # mean CHO
+  x_CHO <- mean(pull(db_5min["CHO_Frayn"]))
+  # mean FAT
+  x_FAT <- mean(pull(db_5min["FAT_Frayn"]))
+  # mean Kcal
+  x_Kcal <- mean(pull(db_5min["Kcal_total_Frayn"]))
+
+  return(list(db = db,
+              db_5min = db_5min,
+              x_VO2 = x_VO2,
+              #x_HR = x_HR,
+              x_RER = x_RER,
+              x_CHO = x_CHO,
+              x_FAT = x_FAT,
+              x_Kcal = x_Kcal))
+}
+
+
+#
+#' Calculation of CHO, FAT and Kcal
+#'
+#' @param step_time how often the data was collected (in seconds).
+#' @param db_MFO dtabase with MFO test
+#' @param VO2max maximum oxygen uptake
+#' @param author eithe "Frayn" or "Jeukendrup"
+#'
+#'
+calculate_vars <- function(step_time, db_MFO, VO2max, author) {
+
+  steps <- calculate_steps(step_time = step_time,
+                           db = db_MFO,
+                           db_type = "MFO")
+
+  db_vars <- data.frame(matrix(ncol = 5, nrow = steps$n_steps))
+  colnames(db_vars) <- c("VO2", "HR", "CHO", "FAT", "Kcal")
+
+  if(author == "Frayn") {
+    # CHO_Frayn (4.55 * VCO2 - 3.21 * VO2) / 1000
+    # FAT_Frayn (1.67 * VO2 - 1.67 * VCO2) / 1000
+    # Kcal_total_Frayn - CHO_Frayn * 4 + FAT_Frayn * 9
+    db_MFO <- db_MFO %>%
+      mutate(CHO_Frayn = (4.55 * VCO2 - 3.21 * VO2) / 1000,
+             FAT_Frayn = (1.67 * VO2 - 1.67 * VCO2) / 1000,
+             Kcal_total_Frayn = CHO_Frayn * 4 + FAT_Frayn * 9)
+
+
+    for (i in seq(1:steps$n_steps)) {
+
+      # mean VO2
+      db_vars[i, "VO2"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "VO2"]))
+      # mean HR
+      db_vars[i, "HR"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "HR"]))
+      # mean CHO
+      db_vars[i, "CHO"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "CHO_Frayn"]))
+      # mean FAT
+      db_vars[i, "FAT"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "FAT_Frayn"]))
+      # mean Kcal
+      db_vars[i, "Kcal"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "Kcal_total_Frayn"]))
+
+      # next bounds
+      if(step_time == 20){
+        steps$upper_bound <- (9 * (i+1))
+        steps$lower_bound <- steps$upper_bound - 3
+      }
+    }
+
+  }  else if (author == "Jeukendrup") {
+
+    # VO2 < 50% VO2max
+    # CHO_Jeukendrup (4.344 * VCO2 - 3.061 * VO2) / 1000
+    # FAT_Jeukendrup (1.695 * VO2 - 1.701 * VCO2) / 1000
+    # Kcal_total_Jeukendrup - CHO_Jeukendrup * 4 + FAT_Jeukendrup * 9
+    # VO2 > 50% VO2max
+    # CHO_Jeukendrup (4.210 * VCO2 - 2.962 * VO2) / 1000
+    # FAT_Jeukendrup (1.695 * VO2 - 1.701 * VCO2) / 1000
+    # Kcal_total_Jeukendrup - CHO_Jeukendrup * 4 + FAT_Jeukendrup * 9
+
+    db_MFO <- db_MFO %>%
+      mutate(CHO_Jeukendrup_40_50_VO2 = (4.344 * VCO2 - 3.061 * VO2) / 1000,
+             FAT_Jeukendrup_40_50_VO2 = (1.695 * VO2 - 1.701 * VCO2) / 1000,
+             Kcal_total_Jeukendrup_40_50_VO2 = CHO_Jeukendrup_40_50_VO2 * 4 + FAT_Jeukendrup_40_50_VO2 * 9,
+             CHO_Jeukendrup_50_75_VO2 = (4.210 * VCO2 - 2.962 * VO2) / 1000,
+             FAT_Jeukendrup_50_75_VO2 = (1.695 * VO2 - 1.701 * VCO2) / 1000,
+             Kcal_total_Jeukendrup_50_75_VO2 = CHO_Jeukendrup_50_75_VO2 * 4 + FAT_Jeukendrup_50_75_VO2 * 9)
+
+    for (i in seq(1:steps$n_steps)) {
+
+      # mean VO2
+      db_vars[i, "VO2"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "VO2"]))
+      # mean HR
+      db_vars[i, "HR"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "HR"]))
+
+      if((db_vars[i, "VO2"] / VO2max) < 50) {
+        # mean CHO
+        db_vars[i, "CHO"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "CHO_Jeukendrup_40_50_VO2"]))
+        # mean FAT
+        db_vars[i, "FAT"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "FAT_Jeukendrup_40_50_VO2"]))
+        # mean Kcal
+        db_vars[i, "Kcal"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "Kcal_total_Jeukendrup_40_50_VO2"]))
+      } else {
+        # mean CHO
+        db_vars[i, "CHO"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "CHO_Jeukendrup_50_75_VO2"]))
+        # mean FAT
+        db_vars[i, "FAT"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "FAT_Jeukendrup_50_75_VO2"]))
+        # mean Kcal
+        db_vars[i, "Kcal"] <- mean(as.vector(db_MFO[steps$lower_bound:steps$upper_bound, "Kcal_total_Jeukendrup_50_75_VO2"]))
+      }
+
+      # next bounds
+      if(step_time == 20){
+        steps$upper_bound <- (9 * (i+1))
+        steps$lower_bound <- steps$upper_bound - 3
+      }
+    }
+
+  }
+
+  return(db_vars)
+
+}
+
+#' Calculate steps
+#'
+#' @param step_time how often the data was collected (in seconds).
+#' @param db a database
+#' @param db_type either "basal" or "MFO"
+#'
+#'
+#' @examples
+calculate_steps <- function(step_time, db, db_type) {
+
+  if(db_type == "basal") {
+    if(step_time == 20){
+      n_steps <- 14
+    }
+    return(n_steps)
+  }
+
+  if(db_type == "MFO") {
+    if(step_time == 20){
+      n_steps <- nrow(db)/9
+      upper_bound <- 9
+      lower_bound <- upper_bound - 3
+    }
+    return(list(n_steps = n_steps,
+                upper_bound = upper_bound,
+                lower_bound = lower_bound))
+  }
+}
+
+#' Get a 5 minutes database
+#'
+#' @param db a database
+#' @param cv_var variable to calculate coefficient of variation
+#' @param n_row number of rows
+#'
+#'
+get_5min <- function(db, cv_var, n_row) {
+
+  pos_final <- which.min(as.vector(db[,cv_var]))
+  pos_ini <- which.min(as.vector(db[,cv_var])) - n_row
+
+  db_5min <- db[pos_ini:pos_final,]
+
+  return(db_5min)
+
+}
